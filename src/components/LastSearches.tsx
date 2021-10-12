5a934a8ffeaea157a3fd50496ad201e7c4f1d8f1
Szymon <sz.abramowski@gmail.com>
2021-10-13 01:05:12 +0200

    add click handler to fetch weather data

--- src/components/LastSearches.tsx
+++ src/components/LastSearches.tsx
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../state/store";
 import { NavDropdown } from "react-bootstrap";
+import { fetchWeatherByCity, resetWeatherState } from "../state";
 
 export default function LastSearches() {
   const [searchHistory, setSearchHistory] = useState<string[]>([]);
   const history = useSelector((state: RootState) => state.history.history);
+  const dispatch = useDispatch();
+
+  function handleClick(city: string) {
+    dispatch(resetWeatherState());
+    dispatch(fetchWeatherByCity(city));
+  }
 
   useEffect(() => {
     setSearchHistory(history);
@@ -16,7 +23,9 @@ export default function LastSearches() {
       {searchHistory !== [] ? (
         searchHistory.map((city: string) => (
           <div key={city + Math.random()}>
-            <NavDropdown.Item>{city}</NavDropdown.Item>
+            <NavDropdown.Item onClick={() => handleClick(city)}>
+              {city}
+            </NavDropdown.Item>
           </div>
         ))
       ) : (
